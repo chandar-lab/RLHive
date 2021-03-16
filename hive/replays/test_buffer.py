de493233704e114f8c27ba6eb35b0575dd1d018a
Your Name <hadinekoei94@gmail.com>
2021-03-16 11:55:19 -0400

    now using pytest.fixture

--- hive/replays/test_buffer.py
+++ hive/replays/test_buffer.py
@@ -4,29 +4,35 @@ import os
 import numpy as np
 from hive import envs, replays
 
-environment = envs.GymEnv('CartPole-v1')
-rng = np.random.default_rng(seed=100)
-buffer = replays.CircularReplayBuffer(rng, size=500, compress=True)
 
-observation, _ = environment.reset()
-for i in range(400):
-    action = rng.integers(environment._env_spec.act_dim)
-    next_observation, reward, done, turn, info = environment.step(action)
-    buffer.add((observation,
-                action,
-                reward,
-                next_observation,
-                done))
-    observation = next_observation
-    if done:
-        observation, _ = environment.reset()
+@pytest.fixture()
+def initialize_buffer():
+    environment = envs.GymEnv('CartPole-v1')
+    rng = np.random.default_rng(seed=100)
+    buffer = replays.CircularReplayBuffer(rng, size=500, compress=True)
+
+    observation, _ = environment.reset()
+    for i in range(400):
+        action = rng.integers(environment._env_spec.act_dim)
+        next_observation, reward, done, turn, info = environment.step(action)
+        buffer.add((observation,
+                    action,
+                    reward,
+                    next_observation,
+                    done))
+        observation = next_observation
+        if done:
+            observation, _ = environment.reset()
+
+    return buffer, environment, rng
 
 
-def test_add_to_buffer():
+@pytest.fixture()
+def test_add_to_buffer(initialize_buffer):
     """
         test adding one transition to the buffer
     """
-
+    buffer, environment, rng = initialize_buffer
     observation, _ = environment.reset()
     action = rng.integers(environment._env_spec.act_dim)
     next_observation, reward, done, turn, info = environment.step(action)
@@ -39,11 +45,11 @@ def test_add_to_buffer():
 
 
 @pytest.mark.parametrize("batch_size", [32])
-def test_sample_from_buffer(batch_size):
+def test_sample_from_buffer(batch_size, initialize_buffer):
     """
     test sampling a batch from the buffer
     """
-
+    buffer, environment, _ = initialize_buffer
     batch = buffer.sample(batch_size=batch_size)
     assert batch["observations"].shape == (batch_size, 4)
     assert batch["actions"].shape == (batch_size,)
@@ -54,21 +60,21 @@ def test_sample_from_buffer(batch_size):
     assert batch["observations"].shape == (buffer.size(), 4)
 
 
-def test_saving_buffer(tmpdir):
+def test_saving_buffer(tmpdir, initialize_buffer):
     """
         test sampling a batch from the buffer
     """
-
+    buffer, environment, _ = initialize_buffer
     buffer.save(tmpdir.mkdir("saved_test_buffer"))
     assert os.path.exists(tmpdir / "saved_test_buffer") is True
 
 
 @pytest.mark.parametrize("batch_size", [32])
-def test_loading_buffer(tmpdir, batch_size):
+def test_loading_buffer(tmpdir, batch_size, initialize_buffer):
     """
         test sampling a batch from the buffer
     """
-
+    buffer, environment, rng = initialize_buffer
     buffer.save(tmpdir / "saved_test_buffer")
     assert os.path.exists(tmpdir / "saved_test_buffer") is True
 
