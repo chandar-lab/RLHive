6f87e877da82c2bbc38cf953e57b4db79405b2eb
sai25 <sai@ai-r.com>
2021-04-02 14:02:26 -0400

    add dueling DQN

--- hive/agents/__init__.py
+++ hive/agents/__init__.py
@@ -1,6 +1,10 @@
 from hive.agents.agent import Agent
 from hive.agents.dqn import DQNAgent
 from hive.agents.double_dqn import DoubleDQNAgent
+from hive.agents.dueling_dqn import DuelingDQNAgent
 from hive.utils.utils import create_class_constructor
 
-get_agent = create_class_constructor(Agent, {"DQNAgent": DQNAgent, "DoubleDQNAgent": DoubleDQNAgent})
+get_agent = create_class_constructor(Agent, {"DQNAgent": DQNAgent,
+                                             "DoubleDQNAgent": DoubleDQNAgent,
+                                             "DuelingDQNAgent": DuelingDQNAgent}
+                                     )
--- hive/agents/qnets/mlp.py
+++ hive/agents/qnets/mlp.py
@@ -19,3 +19,52 @@ class SimpleMLP(nn.Module):
         x = self.input_layer(x)
         x = self.hidden_layers(x)
         return self.output_layer(x)
+
+
+class DuelingMLP(nn.Module):
+    """Simple MLP function approximator for Q-Learning."""
+
+    def __init__(self, in_dim, out_dim, hidden_units=256, num_hidden_layers=1):
+        super().__init__()
+        self.out_dim = out_dim
+        self.input_layer = nn.Sequential(nn.Linear(in_dim[0], hidden_units), nn.ReLU())
+        self.hidden_layers = nn.Sequential(
+            *[
+                nn.Sequential(nn.Linear(hidden_units, hidden_units), nn.ReLU())
+                for _ in range(num_hidden_layers - 1)
+            ]
+        )
+        self.fc1_adv = nn.Linear(hidden_units, hidden_units)
+        self.fc2_adv = nn.Linear(hidden_units, out_dim)
+
+        self.fc1_val = nn.Linear(hidden_units, hidden_units)
+        self.fc2_val = nn.Linear(hidden_units, 1)
+
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.input_layer(x)
+        x = self.hidden_layers(x)
+
+        adv = self.relu(self.fc1_adv(x))
+        adv = self.fc2_adv(adv)
+
+        val = self.relu(self.fc1_val(x))
+        val = self.fc2_val(val)
+
+        # print("val shape = ", val.shape)
+        # print("adv shape = ", adv.shape)
+        # print("adv mean shape = ", adv.mean(1).shape)
+        if len(adv.shape) == 1:
+            print("val shape = ", val.shape)
+            print("adv shape = ", adv.shape)
+            print("adv mean shape = ", adv.mean(0).shape)
+            print("val = ", val)
+            print("adv = ", adv)
+            print("mean = ", adv.mean(0))
+            x = val + adv - adv.mean(0)
+            print("x shape = ", x.shape)
+            print("x = ", x)
+        else:
+            x = val + adv - adv.mean(1).unsqueeze(1).expand(x.shape[0], self.out_dim)
+        return x
