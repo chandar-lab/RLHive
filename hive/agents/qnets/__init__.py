3f8a75e6faf67b0a2a87fca49538876f227def89
sai25 <sai@ai-r.com>
2021-05-12 18:19:47 -0400

    move complex and distributional MLP to rainbow_mlp.py

--- hive/agents/qnets/__init__.py
+++ hive/agents/qnets/__init__.py
@@ -1,5 +1,6 @@
 import torch
-from hive.agents.qnets.mlp import SimpleMLP, ComplexMLP, DistributionalMLP
+from hive.agents.qnets.mlp import SimpleMLP
+from hive.agents.qnets.rainbow_mlp import ComplexMLP, DistributionalMLP
 from hive.utils.utils import create_class_constructor
 
 get_qnet = create_class_constructor(torch.nn.Module, {"SimpleMLP": SimpleMLP,
