aec7fa8458434f907b2afb986c6778bc5872f1fe
Your Name <hadinekoei94@gmail.com>
2021-05-31 19:31:07 -0400

    black format

--- hive/envs/marlgrid/ma_envs/checkers.py
+++ hive/envs/marlgrid/ma_envs/checkers.py
@@ -197,7 +197,7 @@ class CheckersMultiGrid(MultiGridEnvHive):
         )
 
         obs = [self.gen_agent_obs(agent) for agent in self.agents]
-        
+
         # Team reward
         step_rewards = np.array([np.sum(step_rewards) for _ in self.agents])
 
