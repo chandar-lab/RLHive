2b34cfd8f9f7f585466fc4d9e99c55deb7b38817
Amit Sinha <amitfishy@gmail.com>
2021-06-11 16:14:41 -0400

    Removed small typo

--- hive/agents/qnets/__init__.py
+++ hive/agents/qnets/__init__.py
@@ -1,5 +1,5 @@
 import torch
-tils.utils import create_class_constructor
+
 from hive.agents.qnets.mlp import SimpleMLP, DiscObsSimpleMLP
 from hive.agents.qnets.conv import SimpleConvModel
 from hive.agents.qnets.atari import NatureAtariDQNModel
