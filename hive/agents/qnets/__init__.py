34fe5e748f06d5f7e9ff47d607926d1283f99e9c
sai25 <sai@ai-r.com>
2021-06-29 11:38:56 -0400

    minor fix

--- hive/agents/qnets/__init__.py
+++ hive/agents/qnets/__init__.py
@@ -3,7 +3,6 @@ from hive.agents.qnets.mlp import SimpleMLP
 from hive.agents.qnets.rainbow_mlp import ComplexMLP, DistributionalMLP
 from hive.agents.qnets.conv import SimpleConvModel
 from hive.agents.qnets.atari import NatureAtariDQNModel
-from hive.agents.qnets.minatar import MinAtarDQNModel
 from hive.utils.utils import create_class_constructor
 
 get_qnet = create_class_constructor(
