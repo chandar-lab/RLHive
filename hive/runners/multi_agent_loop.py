92c005e217f0afff45637933510668c01cc60b95
Darshan Patil <dapatil211@gmail.com>
2021-06-30 16:29:38 -0400

    Moved all logic into main function so argparse code can be reused

--- hive/runners/multi_agent_loop.py
+++ hive/runners/multi_agent_loop.py
@@ -248,7 +248,7 @@ def set_up_experiment(config):
     return runner
 
 
-if __name__ == "__main__":
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config", default="./config.yml")
     parser.add_argument("-a", "--agent-config")
@@ -258,3 +258,7 @@ if __name__ == "__main__":
     config = load_config(args)
     runner = set_up_experiment(config)
     runner.run_training()
+
+if __name__ == "__main__":
+    main()
+    
