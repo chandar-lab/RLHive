bd900bd3134629689a73f8f9fda914566256391e
sai25 <sai@ai-r.com>
2021-07-06 13:46:53 -0400

    clean up

--- hive/envs/minatar/__init__.py
+++ hive/envs/minatar/__init__.py
@@ -1,8 +1,1 @@
 from hive.envs.minatar.minatar import MinAtarEnv
-
-from gym.envs.registration import register
-
-register(
-    id="breakout-v0",
-    entry_point="hive.envs.minatar.minatar:MinAtarEnv",
-)
