c5fd4d96db49fddc0d72a62ec0651b05bceee7fa
Your Name <hadinekoei94@gmail.com>
2021-08-03 12:01:28 -0400

    remove test scripts

--- test_script.sh
+++
@@ -1,6 +0,0 @@
-python -m hive.runners.single_agent_loop -c configs/cartpole_rainbow/config1.yml &
-python -m hive.runners.single_agent_loop -c configs/cartpole_rainbow/config2.yml &
-python -m hive.runners.single_agent_loop -c configs/cartpole_rainbow/config3.yml &
-python -m hive.runners.single_agent_loop -c configs/cartpole_rainbow/config4.yml &
-python -m hive.runners.single_agent_loop -c configs/cartpole_rainbow/config5.yml &
-python -m hive.runners.single_agent_loop -c configs/cartpole_rainbow/config6.yml &
