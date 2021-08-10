491de6cfac9d8a2c875920bf5174294b898ad7f7
Ali Rahimi-Kalahroudi <alirahkay@gmail.com>
2021-08-10 13:47:22 -0400

    Fix test of the agent

--- hive/agents/tests/test_dqn.py
+++ hive/agents/tests/test_dqn.py
@@ -279,7 +279,7 @@ def test_train_step(agent_with_mock_optimizer):
         )
     assert agent_with_mock_optimizer._optimizer.step.call_count == 6
     assert agent_with_mock_optimizer._replay_buffer.size() == 7
-    assert agent_with_mock_optimizer._epsilon_schedule._value == pytest.approx(0.775)
+    assert agent_with_mock_optimizer._epsilon_schedule._value == pytest.approx(0.82)
 
 
 def test_eval_step(agent_with_mock_optimizer):
