6aea3098498587c88380868a73b9a07ff040eab4
Darshan Patil <dapatil211@gmail.com>
2021-08-17 02:41:04 -0700

    Added std_init parameter

--- hive/agents/qnets/mlp.py
+++ hive/agents/qnets/mlp.py
@@ -1,3 +1,4 @@
+from functools import partial
 from hive.agents.qnets.noisy_linear import NoisyLinear
 import numpy as np
 import torch
@@ -7,12 +8,12 @@ from torch import nn
 class MLPNetwork(nn.Module):
     """Simple MLP function approximator for Q-Learning."""
 
-    def __init__(self, in_dim, hidden_units=256, noisy=False):
+    def __init__(self, in_dim, hidden_units=256, noisy=False, std_init=0.5):
         super().__init__()
         if isinstance(hidden_units, int):
             hidden_units = [hidden_units]
         modules = [torch.nn.Linear(np.prod(in_dim), hidden_units[0])]
-        linear_fn = NoisyLinear if noisy else nn.Linear
+        linear_fn = partial(NoisyLinear, std_init=std_init) if noisy else nn.Linear
         for i in range(len(hidden_units) - 1):
             modules.append(torch.nn.ReLU())
             modules.append(linear_fn(hidden_units[i], hidden_units[i + 1]))
