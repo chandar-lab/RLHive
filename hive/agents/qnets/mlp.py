1836b2c73ed17b70ca7806e7039b7f778152cf82
Darshan Patil <dapatil211@gmail.com>
2021-08-17 02:21:05 -0700

    Refactored qnets to use encapsulation to implement rainbow features, not inheritance

--- hive/agents/qnets/mlp.py
+++ hive/agents/qnets/mlp.py
@@ -1,26 +1,23 @@
+from hive.agents.qnets.noisy_linear import NoisyLinear
 import numpy as np
 import torch
 from torch import nn
 
 
-class SimpleMLP(nn.Module):
+class MLPNetwork(nn.Module):
     """Simple MLP function approximator for Q-Learning."""
 
-    def __init__(self, in_dim, out_dim, hidden_units=256, num_hidden_layers=1):
+    def __init__(self, in_dim, hidden_units=256, noisy=False):
         super().__init__()
-        self.input_layer = nn.Sequential(
-            nn.Linear(np.prod(in_dim), hidden_units), nn.ReLU()
-        )
-        self.hidden_layers = nn.Sequential(
-            *[
-                nn.Sequential(nn.Linear(hidden_units, hidden_units), nn.ReLU())
-                for _ in range(num_hidden_layers - 1)
-            ]
-        )
-        self.output_layer = nn.Linear(hidden_units, out_dim)
+        if isinstance(hidden_units, int):
+            hidden_units = [hidden_units]
+        modules = [torch.nn.Linear(np.prod(in_dim), hidden_units[0])]
+        linear_fn = NoisyLinear if noisy else nn.Linear
+        for i in range(len(hidden_units) - 1):
+            modules.append(torch.nn.ReLU())
+            modules.append(linear_fn(hidden_units[i], hidden_units[i + 1]))
+        self.network = torch.nn.Sequential(*modules)
 
     def forward(self, x):
         x = torch.flatten(x, start_dim=1)
-        x = self.input_layer(x)
-        x = self.hidden_layers(x)
-        return self.output_layer(x)
+        return self.network(x)
