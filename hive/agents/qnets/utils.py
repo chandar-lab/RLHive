2ac8ec493890e730ca977487e4bf7226659fec3a
dapatil211 <dapatil211@gmail.com>
2021-09-07 01:11:17 -0400

    Update utils.py

--- hive/agents/qnets/utils.py
+++ hive/agents/qnets/utils.py
@@ -3,9 +3,7 @@ import torch
 
 def calculate_output_dim(net, input_dim):
     if isinstance(input_dim, int):
-        input_dim = tuple(
-            input_dim,
-        )
+        input_dim = tuple(input_dim)
     placeholder = torch.zeros((0,) + tuple(input_dim))
     output = net(placeholder)
     return output.size()[1:]
