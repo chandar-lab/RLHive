96d8d733f104343951747b8d44fb9e1707b7aff2
Darshan Patil <dapatil211@gmail.com>
2021-09-30 17:16:54 -0400

    Added variance scaling initializer from Tensorflow equivalent

--- hive/agents/qnets/utils.py
+++ hive/agents/qnets/utils.py
@@ -1,5 +1,6 @@
 import torch
 from hive.utils.utils import CallableType, registry
+import math
 
 
 def calculate_output_dim(net, input_dim):
@@ -20,6 +21,33 @@ def create_init_weights_fn(initialization_fn):
         return init_weights
     else:
         return lambda m: None
+
+
+def calculate_correct_fan(tensor, mode):
+    fan_in, fan_out = torch.nn.init._calculate_fan_in_and_fan_out(tensor)
+    if mode == "fan_in":
+        return fan_in
+    elif mode == "fan_out":
+        return fan_out
+    elif mode == "fan_avg":
+        return (fan_in + fan_out) / 2
+    else:
+        raise ValueError(f"Fan mode {mode} not supported")
+
+
+def variance_scaling_(tensor, scale=1.0, mode="fan_in", distribution="uniform"):
+    fan = calculate_correct_fan(tensor, mode)
+    scale /= fan
+    if distribution == "truncated_normal":
+        # constant from scipy.stats.truncnorm.std(a=-2, b=2, loc=0., scale=1.)
+        stddev = math.sqrt(scale) / 0.87962566103423978
+        return torch.nn.init.trunc_normal_(tensor, 0.0, stddev, -2 * stddev, 2 * stddev)
+    elif distribution == "untruncated_normal":
+        stddev = math.sqrt(scale)
+        return torch.nn.init.normal_(tensor, 0.0, stddev)
+    else:
+        limit = math.sqrt(3.0 * scale)
+        return torch.nn.init.uniform_(tensor, -limit, limit)
 
 
 class InitializationFn(CallableType):
@@ -44,6 +72,7 @@ registry.register_all(
         "kaiming_normal": InitializationFn(torch.nn.init.kaiming_normal_),
         "orthogonal": InitializationFn(torch.nn.init.orthogonal_),
         "sparse": InitializationFn(torch.nn.init.sparse_),
+        "variance_scaling": InitializationFn(variance_scaling_),
     },
 )
 
