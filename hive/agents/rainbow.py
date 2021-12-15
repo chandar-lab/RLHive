e3850a07f01581fd9821fa1db69349c34888d6f9
Darshan Patil <dapatil211@gmail.com>
2021-12-14 21:16:24 -0500

    Update rainbow.py

--- hive/agents/rainbow.py
+++ hive/agents/rainbow.py
@@ -129,9 +129,7 @@ class RainbowDQNAgent(DQNAgent):
         self._atoms = atoms if self._distributional else 1
         self._v_min = v_min
         self._v_max = v_max
-        self._supports = torch.linspace(
-            self._v_min, self._v_max, self._atoms, device=device
-        )
+
         if loss_fn is None:
             loss_fn = torch.nn.MSELoss
 
@@ -163,7 +161,11 @@ class RainbowDQNAgent(DQNAgent):
             logger=logger,
             log_frequency=log_frequency,
         )
-
+        
+        self._supports = torch.linspace(
+            self._v_min, self._v_max, self._atoms, device=device
+        )
+        
         self._use_eps_greedy = use_eps_greedy
 
     def create_q_networks(self, representation_net):
