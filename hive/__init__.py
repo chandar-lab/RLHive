c8da0a64622807a70f23a0e3bbd4eb276ed4f317
Darshan Patil <dapatil211@gmail.com>
2021-12-16 17:07:39 -0500

    Added __version__

--- hive/__init__.py
+++ hive/__init__.py
@@ -1,2 +1,9 @@
+import os
+
 from hive import agents, envs, replays, runners, utils
 from hive.utils.registry import Registrable, registry
+
+with open(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "version.txt"))
+) as f:
+    __version__ = f.read().strip()
