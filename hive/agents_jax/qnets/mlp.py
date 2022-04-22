7f36b1263cdc153666f39fbe57453f1589adbd93
Maryam Hashemzadeh <m.hashemzadeh.b@gmail.com>
2022-04-22 10:13:42 -0600

    Update mlp.py

--- hive/agents_jax/qnets/mlp.py
+++ hive/agents_jax/qnets/mlp.py
@@ -9,41 +9,35 @@ import numpy as onp
 
 @gin.configurable
 class JaxMLPNetwork(nn.Module):
-  """Jax MLP network """
+    """Jax MLP network """
 
-  min_vals: Union[None, Tuple[float, ...]] = None
-  max_vals: Union[None, Tuple[float, ...]] = None
-  in_dim: Tuple[int]
-  hidden_units: Union[int, List[int]] = 256
-  noisy: bool = False
-  std_init: float = 0.5
+    min_vals: Union[None, Tuple[float, ...]] = None
+    max_vals: Union[None, Tuple[float, ...]] = None
+    in_dim: Tuple[int]
+    hidden_units: Union[int, List[int]] = 256
+    noisy: bool = False
+    std_init: float = 0.5
 
+    if isinstance(hidden_units, int):
+        hidden_units = [hidden_units]
 
-  if isinstance(hidden_units, int):
-      hidden_units = [hidden_units]
+    def setup(self):
 
-  def setup(self):
+        initializer = nn.initializers.xavier_uniform()  ## TODO add NoisyLinear for Jax
 
-    initializer = nn.initializers.xavier_uniform() ## TODO add NoisyLinear for Jax
+        modules = [nn.Dense(features=self.in_dim, kernel_init=initializer)]
 
-    modules = [nn.Dense(features=self.in_dim, kernel_init=initializer)]
+        for i in range(len(self.hidden_units) - 1):
+            modules.append(nn.relu())
+            modules.append(
+                nn.Dense(features=self.hidden_units[i], kernel_init=initializer)
+            )
 
-    for i in range(len(self.hidden_units) - 1):
-      modules.append(nn.relu())
-      modules.append(nn.Dense(features=self.hidden_units[i], kernel_init=initializer))
+        self.network = modules
 
-    self.network = modules
-
-
-  def forward(self, x):
-      x = x.astype(jnp.float32)
-      x = x.reshape((-1))  # flatten
-      for layer in self.network:
-          x = layer(x)
-      return x
-
-
-
-
-
-
+    def forward(self, x):
+        x = x.astype(jnp.float32)
+        x = x.reshape((-1))  # flatten
+        for layer in self.network:
+            x = layer(x)
+        return x
