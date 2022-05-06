ab5ef030058e57f2fb578f671bb196c3dad00d6f
Sriyash <poddarsriyash@gmail.com>
2022-05-06 11:17:42 -0400

    ppo replay added

--- hive/replays/ppo_replay.py
+++ hive/replays/ppo_replay.py
@@ -1,17 +1,17 @@
-import os
-import pickle
-
 import numpy as np
-
 from hive.replays.circular_replay import CircularReplayBuffer
-from hive.utils.utils import create_folder, seeder
 
 class PPOReplayBuffer(CircularReplayBuffer):
-    def __init__(self,
+    """ An extension of the CircularReplayBuffer for on-policy learning algorithms
+    """
+
+    def __init__(
+        self,
         capacity: int = 10000,
         stack_size: int = 1,
         n_step: int = 1,
         gamma: float = 0.99,
+        use_gae: bool = True,
         gae_lambda: float = 0.95,
         observation_shape=(),
         observation_dtype=np.uint8,
@@ -21,27 +21,75 @@ class PPOReplayBuffer(CircularReplayBuffer):
         reward_dtype=np.float32,
         extra_storage_types=None,
         num_players_sharing_buffer: int = None,
-        ):
+    ):
+        """Constructor for CircularReplayBuffer.
+
+        Args:
+            capacity (int): Total number of observations that can be stored in the
+                buffer. Note, this is not the same as the number of transitions that
+                can be stored in the buffer.
+            stack_size (int): The number of frames to stack to create an observation.
+            n_step (int): Horizon used to compute n-step return reward
+            gamma (float): Discounting factor used to compute n-step return reward
+            use_gae (bool): Whether to use generalised advantage estimates for calculating returns
+            gae_lambda (float): Discouting factor used to compute generalised advantage estimation
+            observation_shape: Shape of observations that will be stored in the buffer.
+            observation_dtype: Type of observations that will be stored in the buffer.
+                This can either be the type itself or string representation of the
+                type. The type can be either a native python type or a numpy type. If
+                a numpy type, a string of the form np.uint8 or numpy.uint8 is
+                acceptable.
+            action_shape: Shape of actions that will be stored in the buffer.
+            action_dtype: Type of actions that will be stored in the buffer. Format is
+                described in the description of observation_dtype.
+            action_shape: Shape of actions that will be stored in the buffer.
+            action_dtype: Type of actions that will be stored in the buffer. Format is
+                described in the description of observation_dtype.
+            reward_shape: Shape of rewards that will be stored in the buffer.
+            reward_dtype: Type of rewards that will be stored in the buffer. Format is
+                described in the description of observation_dtype.
+            extra_storage_types (dict): A dictionary describing extra items to store
+                in the buffer. The mapping should be from the name of the item to a
+                (type, shape) tuple.
+            num_players_sharing_buffer (int): Number of agents that share their
+                buffers. It is used for self-play.
+        """
+        
         super().__init__(capacity, stack_size, n_step, gamma, observation_shape, observation_dtype, action_shape, action_dtype, reward_shape, reward_dtype, extra_storage_types, num_players_sharing_buffer)
-        self._observation_shape = observation_shape
-        self._action_shape = action_shape
+        self._use_gae = use_gae
         self._gae_lambda = gae_lambda
         self._sample_cursor = 0
     
     # Taken from https://github.com/vwxyzjn/ppo-implementation-details/blob/main/ppo_shared.py
     def compute_advantages(self, next_value, next_done):
-        lastgaelam = 0
-        for t in reversed(range(self._capacity)):
-            if t == self._capacity - 1:
-                nextnonterminal = 1.0 - next_done
-                nextvalues = next_value
-            else:
-                nextnonterminal = 1.0 - self._storage["done"][t + 1]
-                nextvalues = self._storage["value"][t + 1]
-            delta = self._storage["reward"][t] + self._gamma * nextvalues * nextnonterminal - self._storage["value"][t]
-            self._storage["advantage"][t] = lastgaelam = delta + self._gamma * self._gae_lambda * nextnonterminal * lastgaelam
-        self._storage["return"] = self._storage["advantage"] + self._storage["value"]
+        """ Compute advantages using rewards and value estimates.
 
+        Args:
+            next_value (float): The value of the next observation to be used for bootstrapping
+            next_done  (bool): If next observation is the last observation in the episode
+        """
+        if self._gae:
+            lastgaelam = 0
+            for t in reversed(range(self._capacity)):
+                if t == self._capacity - 1:
+                    nextnonterminal = 1.0 - next_done
+                    nextvalues = next_value
+                else:
+                    nextnonterminal = 1.0 - self._storage["done"][t + 1]
+                    nextvalues = self._storage["values"][t + 1]
+                delta = self._storage["reward"][t] + self._gamma * nextvalues * nextnonterminal - self._storage["values"][t]
+                self._storage["advantages"][t] = lastgaelam = delta + self._gamma * self._gae_lambda * nextnonterminal * lastgaelam
+            self._storage["returns"] = self._storage["advantages"] + self._storage["values"]
+        else:
+            for t in reversed(range(self._capacity)):
+                if t == self._capacity - 1:
+                    nextnonterminal = 1.0 - next_done
+                    next_return = next_value
+                else:
+                    nextnonterminal = 1.0 - self._storage["done"][t + 1]
+                    next_return = self._storage["returns"][t + 1]
+                self._storage["returns"][t] = self._storage["reward"][t] + self._gamma * nextnonterminal * next_return
+            self._storage["advantages"] = self._storage["returns"] - self._storage["values"]
     
     def reset(self):
         self._create_storage(self._capacity, self._specs)
@@ -49,8 +97,8 @@ class PPOReplayBuffer(CircularReplayBuffer):
         self._cursor = 0
         self._num_added = 0
 
-    def _find_valid_random(self):
-        #Find list of all valid indices
+    def _find_valid_indices(self):
+        """Filters invalid indices."""
         self._sample_cursor = 0
         self._valid_indices = self._filter_transitions(np.arange(self._capacity))
         self._valid_indices = self._rng.permutation(self._valid_indices)
@@ -58,6 +106,6 @@ class PPOReplayBuffer(CircularReplayBuffer):
 
     def _sample_indices(self, batch_size):
         """Samples valid indices that can be used by the replay."""
-        indices = self._valid_indices[self._sample_cursor*batch_size:(self._sample_cursor+1)*batch_size] + self._stack_size - 1
+        indices = self._valid_indices[self._sample_cursor*batch_size:(self._sample_cursor+1)*batch_size]
         self._sample_cursor +=1
-        return indices
+        return indices + self._stack_size - 1
