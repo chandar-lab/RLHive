0ec7fa0686c9f47bc2edd56d23bc6378ba3f2543
Darshan Patil <dapatil211@gmail.com>
2022-08-05 19:21:20 -0400

    Get seed from seeder based on group name

--- hive/agents/random.py
+++ hive/agents/random.py
@@ -29,7 +29,7 @@ class RandomAgent(Agent):
         super().__init__(
             observation_space=observation_space, action_space=action_space, id=id
         )
-        self._action_space.seed(seed=seeder.get_new_seed())
+        self._action_space.seed(seed=seeder.get_new_seed("agent"))
 
     @torch.no_grad()
     def act(self, observation):
