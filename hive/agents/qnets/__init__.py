7d31faf1339416c69a77ca79854da94219ed1d0b
sriyash421 <poddarsriyash@gmail.com>
2022-11-23 15:25:10 -0500

    config fixes

--- hive/agents/qnets/__init__.py
+++ hive/agents/qnets/__init__.py
@@ -3,7 +3,6 @@ from hive.agents.qnets.atari import NatureAtariDQNModel
 from hive.agents.qnets.base import FunctionApproximator
 from hive.agents.qnets.conv import ConvNetwork
 from hive.agents.qnets.mlp import MLPNetwork
-from hive.agents.qnets.normalizer import NormalizationFn
 
 registry.register_all(
     FunctionApproximator,
@@ -11,7 +10,6 @@ registry.register_all(
         "MLPNetwork": MLPNetwork,
         "ConvNetwork": ConvNetwork,
         "NatureAtariDQNModel": NatureAtariDQNModel,
-        "NormalizationFn": NormalizationFn,
     },
 )
 
