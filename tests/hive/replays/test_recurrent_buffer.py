ea627e82794a5c2306bb168ffb011b307fafa7e4
Maryam <m.hashemzadeh.b@gmail.com>
2023-01-24 13:59:33 -0500

    add unit tests for rec buffer

--- tests/hive/replays/test_recurrent_buffer.py
+++ tests/hive/replays/test_recurrent_buffer.py
@@ -1,6 +1,7 @@
 
 import numpy as np
 import pytest
+from pytest_lazyfixture import lazy_fixture
 
 from hive.replays.recurrent_replay import RecurrentReplayBuffer
 
@@ -9,7 +10,7 @@ from hive.replays.recurrent_replay import RecurrentReplayBuffer
 
 OBS_SHAPE = (4, 4)
 CAPACITY = 60
-MAX_SEQ_LEN = 20
+MAX_SEQ_LEN = 10
 N_STEP_HORIZON = 3
 GAMMA = 0.99
 
@@ -20,8 +21,7 @@ def rec_buffer():
         max_seq_len = MAX_SEQ_LEN,
         observation_shape=OBS_SHAPE,
         observation_dtype=np.float32,
-        # extra_storage_types={"priority": (np.int8, ())},
-
+        extra_storage_types={"priority": (np.int8, ())},
     )
 
 @pytest.fixture(
@@ -65,4 +65,85 @@ def full_n_step_buffer():
             done=((i + 1) % 15) == 0,
         )
     return n_step_buffer
+
+
+@pytest.mark.parametrize("constructor", [RecurrentReplayBuffer])
+@pytest.mark.parametrize("observation_shape", [(), (2,), (3, 4)])
+@pytest.mark.parametrize("observation_dtype", [np.uint8, np.float32])
+@pytest.mark.parametrize("action_shape", [(), (5,)])
+@pytest.mark.parametrize("action_dtype", [np.int8, np.float32])
+@pytest.mark.parametrize("reward_shape", [(), (6,)])
+@pytest.mark.parametrize("reward_dtype", [np.int8, np.float32])
+@pytest.mark.parametrize("extra_storage_types", [None, {"foo": (np.float32, (7,))}])
+
+
+def test_constructor(
+    constructor,
+    observation_shape,
+    observation_dtype,
+    action_shape,
+    action_dtype,
+    reward_shape,
+    reward_dtype,
+    extra_storage_types,
+):
+    buffer = constructor(
+        capacity=10,
+        max_seq_len=MAX_SEQ_LEN,
+        observation_shape=observation_shape,
+        observation_dtype=observation_dtype,
+        action_shape=action_shape,
+        action_dtype=action_dtype,
+        reward_shape=reward_shape,
+        reward_dtype=reward_dtype,
+        extra_storage_types=extra_storage_types,
+    )
+    assert buffer.size() == 0
+    assert buffer._max_seq_len == 10
+    assert buffer._storage["observation"].shape == (10,) + observation_shape
+    assert buffer._storage["observation"].dtype == observation_dtype
+    assert buffer._storage["action"].shape == (10,) + action_shape
+    assert buffer._storage["action"].dtype == action_dtype
+    assert buffer._storage["reward"].shape == (10,) + reward_shape
+    assert buffer._storage["reward"].dtype == reward_dtype
+    if extra_storage_types is not None:
+        for key in extra_storage_types:
+            assert buffer._storage[key].shape == (10,) + extra_storage_types[key][1]
+            assert buffer._storage[key].dtype == extra_storage_types[key][0]
+
+
+def test_add(buffer):
+    assert buffer.size() == 0
+    for i in range(CAPACITY):
+        buffer.add(
+            observation=np.ones(OBS_SHAPE) * i,
+            action=i,
+            reward=i % 10,
+            done=((i + 1) % 15) == 0,
+            priority=(i % 10) + 1,
+        )
+        if i < MAX_SEQ_LEN:
+            assert buffer.size() == i
+            assert buffer._cursor == ((i + 1) % CAPACITY)
+        else:
+            assert buffer.size() == i - MAX_SEQ_LEN
+            assert buffer._cursor == ((i + 1) % CAPACITY)
+
+    for i in range(20):
+        buffer.add(
+            observation=np.ones(OBS_SHAPE) * i,
+            action=i,
+            reward=i % 10,
+            done=((i + 1) % 15) == 0,
+            priority=(i % 10) + 1,
+        )
+        assert buffer.size() == CAPACITY - MAX_SEQ_LEN- 1
+        assert buffer._cursor == ((i + 1) % CAPACITY)
+    assert buffer._num_added == CAPACITY + 20
+
+
+
+
+
+
 
