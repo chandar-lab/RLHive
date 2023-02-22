18e378024400bfe110869f3b1765bec64b52bb86
Maryam <m.hashemzadeh.b@gmail.com>
2023-02-22 13:03:27 -0500

    Reformat with black ==23.1.0

--- hive/envs/marlgrid/ma_envs/checkers.py
+++ hive/envs/marlgrid/ma_envs/checkers.py
@@ -85,7 +85,6 @@ class CheckersMultiGrid(MultiGridEnvHive):
             agent.step_reward = 0
 
             if agent.active:
-
                 cur_pos = agent.pos[:]
                 cur_cell = self.grid.get(*cur_pos)
                 fwd_pos = agent.front_pos[:]
